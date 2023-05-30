04f0a338cfd676103bb8e4ee24fdee8ee5f94293
Saleh Alkhamees <101878895+saleh-kh@users.noreply.github.com>
2023-05-30 17:09:57 +0300

    Additions to list.py and todo.py
    Added endpoints for deleting and updating lists.
    
    Added an endpoint to get shared todos.

--- routers/list.py
+++ routers/list.py
@@ -6,11 +6,12 @@ from sqlalchemy.orm import Session
 from pydantic import BaseModel ,Field 
 from .auth import get_current_user
 from typing import List
-from models import Lists
+from models import Lists , Todos
+from sqlalchemy import ARRAY , String , text
 
 
 
-router = APIRouter()
+router = APIRouter(prefix='/list',tags=['list'])
 
 
 def get_db():
@@ -32,10 +33,45 @@ class list_request(BaseModel):
 
 
 #create a new list
-@router.post('/',status_code=status.HTTP_201_CREATED)
+@router.post('/create',status_code=status.HTTP_201_CREATED)
 async def create_list(db:db_dependency, user : user_dependency,new_list:list_request ):
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='auth failed')
     list_model = Lists(**new_list.dict(),list_owner = user.get('id'))
     db.add(list_model)
     db.commit()
+
+
+#delete a list
+@router.delete('/delete')
+async def delete_list(db:db_dependency , user:user_dependency , list_name:str):
+    list = db.query(Lists).filter(Lists.list_owner == user.get('id'),Lists.listname == list_name).first()
+    if not list:
+        raise HTTPException(status_code=404 , detail=f'there\' no list called {list_name}')
+    db.query(Todos).filter(Todos.owner_list == list.id).delete()
+    db.delete(list)
+    db.commit()
+
+#update the list
+@router.put('/update')
+async def update_list(db:db_dependency, user: user_dependency , list_name:str ,updated_list:list_request):
+    list = db.query(Lists).filter(Lists.list_owner == user.get('id'),Lists.listname == list_name).first()
+    if not list:
+        raise HTTPException(status_code=404 , detail=f'there\' no list called {list_name}')
+    list.listname = updated_list.listname 
+    exsiting_shared_with = list.shared_with or [] # exsisitng shared list or empty list if none 
+    new_shared_with = exsiting_shared_with + updated_list.shared_with
+    list.shared_with = new_shared_with
+
+    db.add(list)
+    db.commit()
+
+
+
+# get lists shared with the user
+@router.get('/shared_with_me')
+async def get_shared_with_me(db: db_dependency, user: user_dependency):
+    shared_lists = db.query(Lists).filter(text(":username = ANY(shared_with)").params(username=user.get('username'))).all()
+
+    list_names = [shared_list.listname for shared_list in shared_lists]
+    return list_names
